12b959c1ac45f9486c02cfcf6ff781bc7ee6ac73
Joshua Cole <joshuacole356@gmail.com>
2020-04-29 22:37:45 -0700

    Add UTF characters for the cards

--- app/javascript/channels/game_notifications_channel.js
+++ app/javascript/channels/game_notifications_channel.js
@@ -30,15 +30,23 @@ const socket = consumer.subscriptions.create("GameNotificationsChannel", {
   },
 });
 
+const symbols = {
+  c: "\u2663",
+  d: "\u2666",
+  h: "\u2665",
+  s: "\u2660",
+};
+
 const renderCard = ({ suit, value }) => {
+  const parsedSuit = suit.toLowerCase();
   const suitDiv = document.createElement("div");
-  suitDiv.className = `suit-${suit.toLowerCase()}`;
-  suitDiv.append(suit);
+  suitDiv.className = `suit-${parsedSuit}`;
+  suitDiv.append(symbols[parsedSuit]);
   const valueDiv = document.createElement("div");
   valueDiv.className = "value";
   valueDiv.append(value);
   const div = document.createElement("div");
-  div.className = `card ${suit.toLowerCase()}`;
+  div.className = `card ${parsedSuit}`;
   div.append(valueDiv);
   div.append(suitDiv);
   return div;
