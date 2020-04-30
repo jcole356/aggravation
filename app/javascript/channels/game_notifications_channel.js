67c55a29cd801905d57e2f3ff84363a3b08fefd8
Joshua Cole <joshuacole356@gmail.com>
2020-04-29 21:53:03 -0700

    Render cards for each player

--- app/javascript/channels/game_notifications_channel.js
+++ app/javascript/channels/game_notifications_channel.js
@@ -17,6 +17,7 @@ const socket = consumer.subscriptions.create("GameNotificationsChannel", {
     console.log("data", type);
     switch (type) {
       case "render": {
+        console.log("state", state);
         showPlayers(state);
         break;
       }
@@ -29,16 +30,43 @@ const socket = consumer.subscriptions.create("GameNotificationsChannel", {
   },
 });
 
+const renderCard = ({ suit, value }) => {
+  const suitDiv = document.createElement("div");
+  suitDiv.className = `suit-${suit.toLowerCase()}`;
+  suitDiv.append(suit);
+  const valueDiv = document.createElement("div");
+  valueDiv.className = "value";
+  valueDiv.append(value);
+  const div = document.createElement("div");
+  div.className = `card ${suit.toLowerCase()}`;
+  div.append(valueDiv);
+  div.append(suitDiv);
+  return div;
+};
+
+const renderHand = (cards) => {
+  const hand = document.createElement("div");
+  hand.className = "hand";
+  cards.forEach((card) => {
+    const div = renderCard(card);
+    hand.append(div);
+  });
+  return hand;
+};
+
 // Find the players div, fill it with player divs
 const showPlayers = (players) => {
   const container = document.getElementsByClassName("players")[0];
   container.innerHTML = null;
   players.forEach((player) => {
     const div = document.createElement("div");
+    div.className = "player-container";
     const span = document.createElement("span");
     span.append(player.label);
+    span.className = "player-label";
     div.append(span);
-    div.append(player.hand);
+    // div.append(player.hand);
+    div.append(renderHand(player.cards));
     container.append(div);
   });
   const button = document.getElementsByClassName("start-game")[0];
