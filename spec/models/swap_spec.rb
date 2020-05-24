41698486e2887e6af6b3a301d4878086977c8fe0
Joshua Cole <joshuacole356@gmail.com>
2020-05-23 18:54:15 -0700

    Fix flaky swap spec

--- spec/models/swap_spec.rb
+++ spec/models/swap_spec.rb
@@ -1,53 +1,61 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Swap' do # rubocop:disable Metrics/BlockLength
-  let!(:game) { build(:game) }
-  let!(:set) { build(:hand_set, num_cards: 3, cards: [card2, card3, wild]) }
-  let!(:player1) { build(:player, name: 'Josh', game: game) }
-  let!(:player2) { build(:player, name: 'Christian', game: game, piles: [set]) }
-  let!(:player3) { build(:player, name: 'Eric', game: game) }
   let(:card1) { build(:card) }
   let(:card2) { build(:card) }
-  let(:card3) { build(:card) }
+  let(:card3) { build(:card, value: Card::VALUES[:six]) }
+  let(:card4) { build(:card, value: Card::VALUES[:seven]) }
+  let(:card5) { build(:card, value: Card::VALUES[:eight]) }
+  let(:card6) { build(:card, value: Card::VALUES[:nine]) }
+  let(:card7) { build(:card, value: Card::VALUES[:ten]) }
+  let(:card8) { build(:card, value: Card::VALUES[:jack]) }
   let(:wild) { build(:wild) }
+  let(:set_cards) { [card1, card2, wild] }
+  let(:hand_cards) { [card3, card4, card5, card6, card7, card8] }
+  let!(:game) { build(:game) }
+  let!(:set1) { build(:hand_set, num_cards: 3, cards: set_cards) }
+  let!(:set2) { build(:hand_set, num_cards: 3, cards: set_cards) }
+  let!(:player1) { build(:player, name: 'Josh', game: game) }
+  let!(:player2) { build(:player, name: 'Christian', game: game, piles: [set2]) }
+  let!(:player3) { build(:player, name: 'Eric', game: game) }
   let(:swap) do
     SetSwap.new(game: game, card1_coords: [0, 1], card2_coords: [1, 0, 2])
   end
+  let(:hand) { build(:player_hand, cards: hand_cards, sets: [set1]) }
 
   before(:each) do
     expect(PlayerHand).to receive(:build_sets)
-    expect(PlayerHand).to receive(:build_sets).and_return([set])
+    expect(PlayerHand).to receive(:build_sets).and_return([set2])
     expect(PlayerHand).to receive(:build_sets)
     game.deal
+    # Change the value of the cards in the hand
+    allow(player1).to receive(:hand).and_return(hand)
   end
 
-  # TODO: this is going to fail a bunch cus the cards are random
-  # mock the entire hand
   describe 'execute' do
-    # Mocking the valid? call because cards are randomized
     before do
       expect(swap).to receive(:valid?).and_return(true)
     end
 
     it "transfers card from a player's pile to another player's hand" do
       value = player1.hand.cards.second.clone
-      expect(player1.hand.cards.length).to eq(11)
-      expect(set.cards.length).to eq(3)
+      expect(player1.hand.cards.length).to eq(hand.cards.length)
+      expect(set2.cards.length).to eq(3)
       swap.execute
-      expect(player1.hand.cards.length).to eq(11)
-      expect(set.cards.length).to eq(3)
+      expect(player1.hand.cards.length).to eq(hand.cards.length)
+      expect(set2.cards.length).to eq(3)
       expect(player1.hand.cards.last).to eq(wild)
-      expect(set.cards.last.matches?(value, true)).to eq(true)
+      expect(set2.cards.last.matches?(value, true)).to eq(true)
     end
   end
 
   describe 'valid' do
     it 'requires the first card to match the set' do
-      expect(swap.valid?(set, card1, wild)).to eq(true)
+      expect(swap.valid?(set2, card1, wild)).to eq(true)
     end
 
     it 'requires the second card to be wild' do
-      expect(swap.valid?(set, card1, card2)).to eq(false)
+      expect(swap.valid?(set2, card1, card2)).to eq(false)
     end
   end
 
@@ -62,13 +70,13 @@ RSpec.describe 'Swap' do # rubocop:disable Metrics/BlockLength
       expect(player1.hand.cards).not_to include(card)
       expect(player1.hand.cards).to include(wild)
       expect(wild.current_value).to eq(nil)
-      expect(set.cards).to include(card)
-      expect(set.cards).not_to include(wild)
+      expect(set2.cards).to include(card)
+      expect(set2.cards).not_to include(wild)
       swap.undo
       expect(player1.hand.cards).to include(card)
-      expect(set.cards).not_to include(card)
+      expect(set2.cards).not_to include(card)
       expect(player1.hand.cards).not_to include(wild)
-      expect(set.cards).to include(wild)
+      expect(set2.cards).to include(wild)
       expect(wild.current_value).not_to eq(nil)
     end
   end
